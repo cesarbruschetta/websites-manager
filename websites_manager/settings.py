3e85c18ba39b0fa230175bc180ab9092df63909b
Cesar Bruschetta <cesar.bruschetta@cwi.com.br>
2017-03-21 12:03:43 -0300

    Ajustes no settings

--- websites_manager/settings.py
+++ websites_manager/settings.py
@@ -60,7 +60,7 @@ TEMPLATES = [
         'DIRS': [
             os.path.join(PROJECT_ROOT_PATH, 'core/templates')
         ],
-        'APP_DIRS': True,
+        # 'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
@@ -129,6 +129,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT_PATH, "core/static"),
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    # 'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
 
 
